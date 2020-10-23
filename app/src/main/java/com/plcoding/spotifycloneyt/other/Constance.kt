4a8bd162337a1c7e1ed5686f66a5ff3e5c4dec72
baha <tytenov99@gmail.com>
2020-10-23 15:49:03 +0600

    SongViewMOdel

--- app/src/main/java/com/plcoding/spotifycloneyt/other/Constance.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/other/Constance.kt
@@ -6,5 +6,6 @@ object Constance {
     const val NOTIFICATION_ID = 1
     const val MEDIA_ROOT_ID = "root_id"
     const val NETWORK_ERROR = "NETWORK_ERROR"
+    const val UPDATE_PLAYER_POSITION_INTERVAL = 100L
 }
 
