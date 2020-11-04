f91679bba3375425c92465ac17c3cdb0be390031
baha <tytenov99@gmail.com>
2020-11-04 18:47:07 +0600

    All working fine(Added splah screen)

--- app/src/main/java/com/plcoding/spotifycloneyt/ui/MainActivity.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/ui/MainActivity.kt
@@ -37,6 +37,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setTheme(R.style.AppTheme)
+
         setContentView(R.layout.activity_main)
         subscribeToObservers()
         vpSong.adapter = swipeSongAdapter
